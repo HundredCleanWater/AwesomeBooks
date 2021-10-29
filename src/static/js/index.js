145f2753e96f743d1bebd237e066670d5309f29c
hamkke <walwalzz@naver.com>
2021-10-29 17:44:05 +0900

    21/10/29

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,21 +1,5 @@
-let submitBtn =  document.querySelector('.submitBtn')
-let search = document.querySelector('.iinput').innerText
 
-function gogo() {
-    $.ajax({
-        type: "GET",
-        url: "/search",
-        data: {search_give: search},
-        success: function (response) {
-            // console.log(response)
-            console.log(123)
-            // let json_search = JSON.parse(response['all_search_results'])
-            // console.log(json_search)
-        }
-    })
-}
 
-submitBtn.addEventListener("click", gogo())
 
 
 
