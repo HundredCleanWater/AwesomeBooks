6999cd54bb042024278104142c80f1cb679c107a
hamkke <walwalzz@naver.com>
2021-10-29 17:28:35 +0900

    21.10.29

--- src/apiRouter/apitest.py
+++ src/apiRouter/apitest.py
@@ -1,27 +1,31 @@
-import json
+from flask import Blueprint, jsonify, request, render_template
 import urllib.request
-import re
-pattern = re.compile(u'<\/?\w+\s*[^>]*?\/?>')
+
+search_page = Blueprint('search', __name__, url_prefix='/')
+
+@search_page.route('/search', methods=['GET'])
+def search():
+    search_receive = request.args.get('search_give')
+    search_result = api_value(search_receive)
+    return jsonify({'all_search_results': search_result})
+
+def api_value(query):
+    client_id = "FX0D1x996cJpBNHf5huf"
+    client_secret = "ZFgUa_ti5y"
+    encText = urllib.parse.quote(query)
+    url = "https://openapi.naver.com/v1/search/book_adv.json?d_catg=280&d_titl=" + encText  # json 결과
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id", client_id)
+    request.add_header("X-Naver-Client-Secret", client_secret)
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
+    if (rescode == 200):
+        response_body = response.read()
+        return response_body.decode('utf-8')
+    else:
+        print("Error Code:" + rescode)
 
 
-client_id = "FX0D1x996cJpBNHf5huf"
-client_secret = "ZFgUa_ti5y"
-encText = urllib.parse.quote("자바")
-url = "https://openapi.naver.com/v1/search/book_adv.json?d_catg=280&d_titl=" + encText  # json 결과
-request = urllib.request.Request(url)
-request.add_header("X-Naver-Client-Id", client_id)
-request.add_header("X-Naver-Client-Secret", client_secret)
-response = urllib.request.urlopen(request)
-rescode = response.getcode()
-if (rescode == 200):
-    response_body = response.read()
-    result = response_body.decode('utf-8')
-    qq = json.loads(result)
-    a = qq['items'][0]['title']
-    a1 = str(a)
-    print(pattern.sub(u"", a1))
 
-else:
-    print("Error Code:" + rescode)
 
 
--- src/views/test_bp.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-test_bp = Blueprint('test_bp', __name__)
-
-@test_bp.route("/")
-def index():
-    name = "제발"
-    return render_template("index.html", data=name)
